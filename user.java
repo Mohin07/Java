a3cbce2cbd486795373a3ac11af4fcb38dde779b
Mohin07 <nadafmohin360@gmail.com>
2025-02-11 02:38:17 -0800

    Add files via upload
    Facebook

---
+++ user.java
@@ -0,0 +1,13 @@
+public class user{
+public static void main(String[] abc){
+	String userName="Mark Zuckerberg";
+	String qualification="BCA";
+	long contactNumber=2345678919L;
+	int age=20;
+	if(age<18){
+		System.out.println("Can't create faceBook account");
+	}else{
+		System.out.println("Can create faceBook account");
+	}
+}
+}
